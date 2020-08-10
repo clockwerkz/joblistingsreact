327022572bc98b6689e84298eb089e64417868c6
Carlos Fins <clockwerkz@gmail.com>
2020-08-10 14:43:37 -0700

    setting up useReducer basic practice with useReducer to re-aquaint myself to the work flow.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import "./App.css";
 
-function App() {
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "ADD":
+      const newTodos = [...state.todos];
+      newTodos.push(payload);
+      return {
+        ...state,
+        todos: newTodos
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [{ todos }, dispatch] = useReducer(reducer, {
+    todos: ["Wash the car"]
+  });
+  const [input, setInput] = useState("");
+
+  const handleSubmitBtn = () => {
+    if (input === "") return;
+    dispatch({ type: "ADD", payload: input });
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleSubmitBtn}>Submit</button>
+      <h1>Hello functional component</h1>
+      {todos.length > 0 ? (
+        <ul>
+          {" "}
+          {todos.map((todo, key) => (
+            <li key={key}>{todo}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Nothing to do yet!</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
