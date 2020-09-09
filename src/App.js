6a236c8264de6c085215e355bffb5aba388ca5d9
Carlos <clockwerkz@gmail.com>
2020-09-09 15:39:54 -0700

    bringing in html structure to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useReducer, useState } from "react";
-import "./App.css";
+import "./sass/main.scss";
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
@@ -38,34 +38,66 @@ const App = () => {
   const handleTodo = (index) => {};
 
   return (
-    <div>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={handleSubmitBtn}>Submit</button>
-      <h1>Hello functional component</h1>
-      {todos.length > 0 ? (
-        <ul>
-          {" "}
-          {todos.map((todo, index) => (
-            <li key={index}>
-              {todo.task}{" "}
-              <input
-                type="checkbox"
-                checked={todo.complete}
-                onChange={(e) =>
-                  dispatch({ type: "UPDATE_TODO", payload: index })
-                }
-              />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Nothing to do yet!</p>
-      )}
-    </div>
+    <section class="search">
+        <div class="search-bar">
+            <div class="search-bar__form-input">
+                <i class="material-icons">work_outline</i>
+                <input type="text" class="search-bar__input" placeholder="Title, companies, expertise or benefits" />
+                <button class="btn">Search</button>
+            </div>
+            
+        </div>
+        <div class="search-body">
+            
+          <aside class="search-criteria">
+              <label class="search-option">Full time
+                  <input type="checkbox" />
+                  <span class="checkmark"></span>
+              </label>
+              <h3 class="search__subtitle">Location</h3>
+              <div class="search-bar__form-input">
+                  <i class="material-icons">public</i>
+                  <input type="text" class="search-bar__input" placeholder="City, state, zip code or country" />
+              </div>
+              <div class="search__cities">
+                <label class="search-option city">London
+                    <input type="checkbox" />
+                    <span class="checkmark"></span>
+                </label>
+                <label class="search-option city">Amsterdam
+                    <input type="checkbox" />
+                    <span class="checkmark"></span>
+                </label>
+                <label class="search-option city">New York
+                    <input type="checkbox" />
+                    <span class="checkmark"></span>
+                </label>
+                <label class="search-option city">Berlin
+                    <input type="checkbox" />
+                    <span class="checkmark"></span>
+                </label>
+              </div>
+          </aside>
+
+          <main class="search-results">
+              <div class="card">
+                  <div class="card__img">not found</div>
+                      <div class="card__body">
+                          <div class="card__info">
+                              <h2 class="card__company">New York University</h2>
+                              <h1 class="card__position">Front-End Software Engineer</h1>
+                              <h3 class="card__highlight">Full time</h3>
+                          </div>
+                          <ul class="card__tags">
+                              <li class="card__tag"><i class="material-icons">public</i> New York</li>
+                              <li class="card__tag"><i class="material-icons">access_time</i> 5 days ago</li>
+                          </ul>
+                  </div> 
+              </div> 
+
+          </main>
+        </div>
+    </section>
   );
 };
 
