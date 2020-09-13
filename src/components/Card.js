c104836495f4fc835b3d49fff5607e9c5a1d1625
Carlos <clockwerkz@gmail.com>
2020-09-13 16:05:25 -0700

    fixes meta tags displaying incorrectly

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,17 +7,20 @@ const Card = ({ id, type, url, created_at, company,company_logo, company_url, lo
     
     return (
         <div className="card">
-            {company_logo ? <img src={company_logo} className="card__img" /> : <div className="card__img">not found</div>}
+            {company_logo ? <div style={{backgroundImage: `url(${company_logo})`}} className="card__img" /> : <div className="card__img">not found</div>}
                 <div className="card__body">
                     <div className="card__info">
-    <h2 className="card__company">{company}</h2>
-    <h1 className="card__position">{title}</h1>
+                        <h2 className="card__company">{company}</h2>
+                        <h1 className="card__position">{title}</h1>
+                        <div className="card__meta">
                         {type==="Full Time" ? <h3 className="card__highlight">Full time</h3> : ""}
+                        <ul className="card__tags">
+                            <li className="card__tag"><i className="material-icons">public</i>{location}</li>
+                            <li className="card__tag"><i className="material-icons">access_time</i>{difference_in_days} days ago</li>
+                        </ul>
                     </div>
-                    <ul className="card__tags">
-    <li className="card__tag"><i className="material-icons">public</i>{location}</li>
-                        <li className="card__tag"><i className="material-icons">access_time</i>{difference_in_days} days ago</li>
-                    </ul>
+                    </div>
+                    
             </div> 
         </div> 
     )
