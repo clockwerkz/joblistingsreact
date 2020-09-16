814c938c65b1b617ed5737e94d781d3ba3235b08
Carlos <clockwerkz@gmail.com>
2020-09-15 18:33:28 -0700

    adds react-pagination

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import connect from '../utilities/connect';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 import Card from './Card';
 
 const SearchResults = ({ results, start, end }) => {
+    const handlePaginationClick = (output) => console.log(output);
+    const paginateProps = {
+        pageCount : 7,
+        containerClassName : 'details__pagination',
+        pageClassName : 'details__pagination-number',
+        pageRangeDisplayed : 2,
+        onPageChange : handlePaginationClick
+    }
     return (
         <main className="search-results">
+            <ReactPaginate {...paginateProps}/>
             {results.length > 0 ? 
             (
                 results.slice(start, end).map(card=>(<Link to={card.id} key={card.id}><Card {...card}/></Link>))
