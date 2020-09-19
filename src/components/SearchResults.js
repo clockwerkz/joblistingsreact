88e4195717c33f36e382d05a01486a8517c6b7be
clockwerkz <clockwerkz@gmail.com>
2020-09-19 00:17:39 -0700

    adding gsap to the project

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import gsap from "gsap";
 import connect from '../utilities/connect';
 import ReactPaginate from 'react-paginate';
 
@@ -32,17 +33,18 @@ const SearchResults = ({ results, start, end, updatePage, cities, locationText, 
     }
 
     useEffect(() => {
-        const geo = navigator.geolocation;
-        if (!geo) {
-            console.error("Geolocation is not supported");
-        } else {
-            geo.getCurrentPosition((geo)=>{
-                console.log(geo.coords);
-                const lat = geo.coords.latitude;
-                const lng = geo.coords.longitude;
-                updateLocationInput({lat, lng});
-            });
-        }
+
+        // const geo = navigator.geolocation;
+        // if (!geo) {
+        //     console.error("Geolocation is not supported");
+        // } else {
+        //     geo.getCurrentPosition((geo)=>{
+        //         console.log(geo.coords);
+                //const lat = geo.coords.latitude;
+                //const lng = geo.coords.longitude;
+                //updateLocationInput({lat, lng});
+        //     });
+        // }
         //window.scrollTo(0, 0)
     },[])
 
