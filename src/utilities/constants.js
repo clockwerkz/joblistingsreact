d3a47ab25f556102e0c91c5e32dcf93011253ed9
Carlos <clockwerkz@gmail.com>
2021-03-07 16:51:58 -0800

    Adds my own cors pass through for cors issues

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,11 +1,11 @@
 const prod = {
     url: {
-        API_URL: 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?',
+        API_URL: 'https://young-thicket-65531.herokuapp.com/https://jobs.github.com/positions.json?',
         }
    };
    const dev = {
     url: {
-        API_URL: 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?'
+        API_URL: 'https://young-thicket-65531.herokuapp.com/https://jobs.github.com/positions.json?'
         }
    };
    export const config = process.env.NODE_ENV === 'development' ? dev : prod;
